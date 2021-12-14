fac4ef10d5579a3e12680af4fe263d646f73ec06
Yatharth Sharma <sharmayatharth.21@gmail.com>
2021-12-14 11:46:45 +0530

    change launching date

--- src/App.js
+++ src/App.js
@@ -1,73 +1,94 @@
-import './App.css';
-import { publicRoutes, privateRoutes } from './routes/index.js';
-import PrivateRoute from './helper/PrivateRoute';
+import "./App.css";
+import { publicRoutes, privateRoutes } from "./routes/index.js";
+import PrivateRoute from "./helper/PrivateRoute";
 import { QueryClient, QueryClientProvider } from "react-query";
-import { useLocation, Route } from 'react-router-dom';
-import { HelmetProvider } from 'react-helmet-async';
-import AuthProvider from './context/AuthContext.jsx';
-import {useCallback, useEffect, useState} from 'react';
-import { ReactQueryDevtools } from 'react-query/devtools'
+import { useLocation, Route } from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async";
+import AuthProvider from "./context/AuthContext.jsx";
+import { useCallback, useEffect, useState } from "react";
+import { ReactQueryDevtools } from "react-query/devtools";
 
 function App() {
-	const queryClient = new QueryClient();
-	const location =  useLocation();
+  const queryClient = new QueryClient();
+  const location = useLocation();
 
-	useEffect( () => { 
-		if(location.pathname.includes('user') || location.pathname.includes('dashboard')) 
-			{
-				document.querySelector("body").classList.add("theme-blush")
-				document.querySelector("body").classList.remove("web_font1")
-			}
-		else{
-			document.querySelector("body").classList.remove("theme-blush")
-			document.querySelector("body").classList.add("web_font1")
-		}
-	});
+  useEffect(() => {
+    if (
+      location.pathname.includes("user") ||
+      location.pathname.includes("dashboard")
+    ) {
+      document.querySelector("body").classList.add("theme-blush");
+      document.querySelector("body").classList.remove("web_font1");
+    } else {
+      document.querySelector("body").classList.remove("theme-blush");
+      document.querySelector("body").classList.add("web_font1");
+    }
+  });
 
-	const [width, setWidth] = useState(window.innerWidth);
+  const [width, setWidth] = useState(window.innerWidth);
 
-	function handleWindowSizeChange() {
-		setWidth(window.innerWidth);
-		if(window.innerWidth < 1170){
-			document.querySelector("body").classList.add("ls-closed")
-		}else{
-			document.querySelector("body").classList.remove("ls-closed", "overlay-open")
-		}
-	}
+  function handleWindowSizeChange() {
+    setWidth(window.innerWidth);
+    if (window.innerWidth < 1170) {
+      document.querySelector("body").classList.add("ls-closed");
+    } else {
+      document
+        .querySelector("body")
+        .classList.remove("ls-closed", "overlay-open");
+    }
+  }
 
-	useEffect(() => {
-			if(width < 1170){
-				document.querySelector("body").classList.add("ls-closed")
-			}else{
-				document.querySelector("body").classList.remove("ls-closed", "overlay-open")
-			}
-			window.addEventListener('resize', handleWindowSizeChange);
-			return () => {
-				window.removeEventListener('resize', handleWindowSizeChange);
-				document.querySelector("body").classList.add("ls-closed")
-			}
-	}, []);
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
 
-	return (
-		<HelmetProvider>
-			{/* <Router> */}
-				<QueryClientProvider client={queryClient}>
-					<AuthProvider>
-						{/* <Switch> */}
-							{privateRoutes && privateRoutes.map((route => (
-								<PrivateRoute exact={true} key={route.path} path={route.path} component={route.component} />
-							)))}
+  useEffect(() => {
+    if (width < 1170) {
+      document.querySelector("body").classList.add("ls-closed");
+    } else {
+      document
+        .querySelector("body")
+        .classList.remove("ls-closed", "overlay-open");
+    }
+    window.addEventListener("resize", handleWindowSizeChange);
+    return () => {
+      window.removeEventListener("resize", handleWindowSizeChange);
+      document.querySelector("body").classList.add("ls-closed");
+    };
+  }, []);
 
-							{publicRoutes && publicRoutes.map((route => (
-								<Route exact={true} key={route.path} path={route.path} component={route.component} />
-							)))}
-							{/* <ReactQueryDevtools initialIsOpen={false}/> */}
-						{/* </Switch> */}
-					</AuthProvider>
-				</QueryClientProvider>
-			{/* </Router> */}
-		</HelmetProvider>
-	);
+  return (
+    <HelmetProvider>
+      {/* <Router> */}
+      <QueryClientProvider client={queryClient}>
+        <AuthProvider>
+          {/* <Switch> */}
+          {privateRoutes &&
+            privateRoutes.map((route) => (
+              <PrivateRoute
+                exact={true}
+                key={route.path}
+                path={route.path}
+                component={route.component}
+              />
+            ))}
+
+          {publicRoutes &&
+            publicRoutes.map((route) => (
+              <Route
+                exact={true}
+                key={route.path}
+                path={route.path}
+                component={route.component}
+              />
+            ))}
+          {/* <ReactQueryDevtools initialIsOpen={false}/> */}
+          {/* </Switch> */}
+        </AuthProvider>
+      </QueryClientProvider>
+      {/* </Router> */}
+    </HelmetProvider>
+  );
 }
 
 export default App;
